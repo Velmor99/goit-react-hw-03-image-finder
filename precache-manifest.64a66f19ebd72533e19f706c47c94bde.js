4fbb9bceb4c4f30b74bff8ea1b17016757e26ab7
velmor99 <volgagaz24100@gmail.com>
2020-08-09 23:49:08 +0300

    Updates

---
+++ precache-manifest.64a66f19ebd72533e19f706c47c94bde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347d85bb5f974ff82ea1875dcfe116af",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "75964db51efdd0dda427",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.fe4326c0.chunk.css"
+  },
+  {
+    "revision": "09bbfe144c4b79d69089",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.03cac52b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.03cac52b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75964db51efdd0dda427",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.a0246163.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
